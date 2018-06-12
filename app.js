cf6fa0dcfc883ad125ae309bb5c1fe437ad3f542
Sushil Sharma <aadisrma26@gmail.com>
2018-06-13 00:08:56 +0530

    removed Extra Space

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 let itemBox = document.querySelector(".item");
 
 //eventListner
+
 eventListner();
 function eventListner(){
     document.querySelector("#todo").addEventListener('submit',fetchingData);
     itemBox.addEventListener('click',removeFun);
 }
-
 
 //function
 
@@ -19,14 +19,12 @@ function fetchingData(e){
     const rvmBtn = document.createElement("a");
     const li = document.createElement("li");
 
-    
     rvmBtn.classList = "removeItem";
     rvmBtn.textContent = "X"
     
     li.textContent = inputValue;
     li.appendChild(rvmBtn);
     itemBox.appendChild(li);
-
 }
 function removeFun(e){
     if(e.target.classList.contains("removeItem")){
