a72884a8327e057f3919e2e264d1bf53d3bc4c61
Sushil Sharma <aadisrma26@gmail.com>
2018-06-13 00:03:25 +0530

    Todo App

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
-let x = document.getElementById("text");
-x.textContent="learning git";
+//Variable
+
+let itemBox = document.querySelector(".item");
+
+//eventListner
+eventListner();
+function eventListner(){
+    document.querySelector("#todo").addEventListener('submit',fetchingData);
+    itemBox.addEventListener('click',removeFun);
+}
+
+
+//function
+
+function fetchingData(e){
+    e.preventDefault();
+    let inputValue = document.getElementById('myValue').value;
+
+    const rvmBtn = document.createElement("a");
+    const li = document.createElement("li");
+
+    
+    rvmBtn.classList = "removeItem";
+    rvmBtn.textContent = "X"
+    
+    li.textContent = inputValue;
+    li.appendChild(rvmBtn);
+    itemBox.appendChild(li);
+
+}
+function removeFun(e){
+    if(e.target.classList.contains("removeItem")){
+        e.target.parentElement.remove();
+    }
+}
